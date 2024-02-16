b9d79bda09436966b3a16cdedc8b01b5934babb1
miroku0000 <49042680+miroku0000@users.noreply.github.com>
2024-02-16 14:13:03 -0800

    Update fix.py

--- fix.py
+++ fix.py
@@ -4,16 +4,19 @@ import os
 
 def replace_string_in_file(file_path, search_string, replace_string):
     try:
+        found=False
         # Read in the file with UTF-8 encoding
         with open(file_path, 'r', encoding='utf-8') as file:
             filedata = file.read()
-        if search_string in filedata:  
+        if search_string in filedata:
+            found=True
             # Replace the target string
             filedata = filedata.replace(search_string, replace_string)
             # Write the file out again
             with open(file_path, 'w', encoding='utf-8') as file:
                 file.write(filedata)
-            os.system("python -m compileall .")
+            #os.system("python -m compileall " + file_path)
+            return True
     except UnicodeDecodeError:
         print(f"Skipping file (not a text file or encoding issue): {file_path}")
     except Exception as e:
@@ -21,11 +24,16 @@ def replace_string_in_file(file_path, search_string, replace_string):
 
 def search_and_replace_in_directory(directory, search_string, replace_string):
     for dirpath, dirnames, filenames in os.walk(directory):
+        found=False
         for filename in filenames:
             if filename.endswith('.py'):
                 file_path = os.path.join(dirpath, filename)
-                replace_string_in_file(file_path, search_string, replace_string)
-
+                r=replace_string_in_file(file_path, search_string, replace_string)
+                if r:
+                    found=True
+        if found:
+             os.system("python -m compileall " + dirpath)
+                    
 # Define the search and replace strings
 search_string = "if self.safety_checker is None"
 replace_string = "if True"
