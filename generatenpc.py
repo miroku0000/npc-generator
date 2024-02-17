ffd241a87acf0133173ff2de52bdc306fd43c89a
miroku0000 <49042680+miroku0000@users.noreply.github.com>
2024-02-16 19:47:57 -0800

    Update generatenpc.py

--- generatenpc.py
+++ generatenpc.py
@@ -10,19 +10,15 @@ import glob
 def write_prompt_to_text_files(directory, prompt):
     # Search for all .png files in the specified directory
     png_files = glob.glob(os.path.join(directory, '*.png'))
-    
     for png_file in png_files:
         # Generate the new filename by adding "_prompt.txt" to the original file name
         base_name = os.path.basename(png_file)  # Get the base name of the file
         new_filename = os.path.splitext(base_name)[0] + "_prompt.txt"  # Remove .png extension and add "_prompt.txt"
         new_filepath = os.path.join(directory, new_filename)  # Create the full path for the new file
-        
         # Write the prompt to the new text file
         with open(new_filepath, 'w') as text_file:
-            text_file.write(prompt)
-            
+            text_file.write(prompt) 
         print(f"Prompt written to {new_filepath}")
-
 
 def create_output_directory(directory="output"):
     # Define the directory path
@@ -135,13 +131,12 @@ def createPicture(desc,steps=16, width=512, height=512, seed=""):
     :param desc: string containing adescription to give the LLM for picture generation
     :param seed: Optionally, providing the seed will get you a known result.    
     """
-	cmd='venv\\Scripts\\activate venv && python main.py ' + '"' + desc+ '"' + " --steps " + str(steps) + " --width " +str(width) + " --height " +  str(height)
+	cmd='python main.py ' + '"' + desc+ '"' + " --steps " + str(steps) + " --width " +str(width) + " --height " +  str(height)
+	#cmd='venv\\Scripts\\activate venv && python main.py ' + '"' + desc+ '"' + " --steps " + str(steps) + " --width " +str(width) + " --height " +  str(height)
 	if seed:
 		print("using seed " +str(seed))
 		cmd = cmd + " --seed " + str(seed)
 	os.system(cmd) 
-
-
 parser=argparse.ArgumentParser(description="sample argument parser")
 parser.add_argument("--npcrace", choices=['human','elf','dwarf','halfing','gnome','orc','random'], default='random')
 parser.add_argument("--npcclass", default='random', choices=['druid','bard','wizard','barbarian','rogue','sorcerer','warlock','cleric','fighter','shopkeep','bartender','whore','innkeeper','random'])
